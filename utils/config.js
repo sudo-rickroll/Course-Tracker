4284d976ca8dc50f4be3090ef54c6ab42202d631
sudo-rickroll <rangasai.kr@gmail.com>
2025-01-19 19:14:11 -0500

    Update config to fetch env variables from host

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const fs = require('fs')
 
-const mongodb_uri = process.env.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST : (process.env.MONGODB_URI_PROD || fs.readFileSync('/etc/secrets/MONGODB_URI', 'utf8').trim())
+const mongodb_uri = process.env.NODE_ENV === 'test' ? (process.env.MONGODB_URI_TEST || fs.readFileSync('/etc/secrets/MONGODB_URI_TEST', 'utf8').trim()) : (process.env.MONGODB_URI_PROD || fs.readFileSync('/etc/secrets/MONGODB_URI_PROD', 'utf8').trim())
 const port = process.env.PORT
 const passwordSaltRounds = process.env.SALT_ROUNDS
-const secret = process.env.SECRET
+const secret = process.env.SECRET || fs.readFileSync('/etc/secrets/SECRET', 'utf8').trim()
 
 module.exports = {
     mongodb_uri,
